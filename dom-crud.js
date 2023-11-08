dd06fabc163338d81958b8e599872abf39c029b6
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2023-11-07 20:47:54 -0800

    Add class name to third li

--- dom-crud.js
+++ dom-crud.js
@@ -1,14 +1,18 @@
-// Create a new <a> element containing the text "Buy Now!" 
+// Create a new <a> element containing the text "Buy Now!"
 // with an id of "cta" after the last <p>
-
 
 // Access (read) the data-color attribute of the <img>,
 // log to the console
 
+const carImg = document.getElementsByTagName("img")[1];
+const dataColor = carImage.dataset.color;
 
-// Update the third <li> item ("Turbocharged"), 
+console.log("dataColor");
+
+// Update the third <li> item ("Turbocharged"),
 // set the class name to "highlight"
-
+const thirdList = document.getElementsByTagName("li")[2];
+thirdList.classList.add(`highlight`);
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
