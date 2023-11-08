4f6b4ad8dd844e9b5f91ab287053cbe18bfd8de4
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2023-11-07 18:46:41 -0800

    color

--- accessing.js
+++ accessing.js
@@ -1,7 +1,8 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
 
-h2.innerText = `February 10 Weather Forecast, Seattle`;
-
 // Change the styling of every element with class "sun" to set the color to "orange"
+
+const link = document.getElementsByClassName("sun");
+link.style.color = "orange";
 
 // Change the class of the second <li> from to "sun" to "cloudy"
