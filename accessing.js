05fc27f0e975b19071359dcde721ba72f8f3a3be
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2023-11-07 19:30:41 -0800

    changed color
    added style to html head to change color of sun elements

--- accessing.js
+++ accessing.js
@@ -1,8 +1,9 @@
 // Change the text of the "Seattle Weather" header to "February 10 Weather Forecast, Seattle"
+head2 = document.getElementById(`weather-head`);
+head2.innerText = `February 10 Weather Forecast, Seattle`;
 
 // Change the styling of every element with class "sun" to set the color to "orange"
 
-const link = document.getElementsByClassName("sun");
-link.style.color = "orange";
-
 // Change the class of the second <li> from to "sun" to "cloudy"
+
+weather = document.getElementsBy;
