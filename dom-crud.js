aa6fbef7913ed4d32caadc1f0c3f040d53f855cf
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2023-11-11 16:19:15 -0800

    finished dom.crud.js
    I added parent and child id to html

--- dom-crud.js
+++ dom-crud.js
@@ -1,18 +1,29 @@
 // Create a new <a> element containing the text "Buy Now!"
 // with an id of "cta" after the last <p>
 
+const para = document.createElement("cta");
+para.id = `cta`;
+const paraText = document.createTextNode("Buy Now!");
+para.appendChild(paraText);
+
+const body = document.getElementsByTagName("body")[0];
+body.appendChild(para);
+
 // Access (read) the data-color attribute of the <img>,
 // log to the console
 
-const carImg = document.getElementsByTagName("img")[1];
-const dataColor = carImage.dataset.color;
+const imgColors = document.querySelector("img").getAttribute("data-color");
 
-console.log("dataColor");
+console.log("imgColors");
 
 // Update the third <li> item ("Turbocharged"),
 // set the class name to "highlight"
+
 const thirdList = document.getElementsByTagName("li")[2];
 thirdList.classList.add(`highlight`);
 
 // Remove (delete) the last paragraph
 // (starts with "Available for purchase now…")
+const bodyElement = document.getElementById("parent");
+const paragraph = document.getElementById("child");
+bodyElement.removeChild(paragraph);
