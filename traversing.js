0f8b670b9df9f45b56d86fe84b94e15d591a43f3
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2023-11-14 17:47:15 -0800

    Access nodes (js)

--- traversing.js
+++ traversing.js
@@ -1,11 +1,15 @@
 // Given the <body> element as variable body,
 // access the <main> node and log to the console.
-const body = document.querySelector('body');
+const body = document.querySelector("body");
+const mainEl = body.getAttribute("main");
+console.log(mainEl);
 
 // Given the <ul> element as variable ul,
 // access <body>  and log to the console.
-const ul = document.querySelector('ul');
+const ul = document.querySelector("ul");
+const bodyEl = body.getAttribute("body");
 
 // Given the <p> element as var p,
 // access the 3rd <li>  and log to the console.
-const p = document.querySelector('p');
+const p = document.querySelector("p", "li", [2]);
+console.log(p);
