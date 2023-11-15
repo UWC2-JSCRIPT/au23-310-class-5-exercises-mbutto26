2c396e20a7c354f86db8f37b392faa3e0a30dafc
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2023-11-14 17:59:33 -0800

    +/- & stoppingBehavior

--- plusesAndMinuses.js
+++ plusesAndMinuses.js
@@ -1,2 +1,10 @@
 // When a user clicks the + element, the count should increase by 1 on screen.
+let plus = 0;
+function buttonClick() {
+  document.getElementById("inc").value = ++i;
+}
 // When a user clicks the – element, the count should decrease by 1 on screen.
+let minus = 0;
+function buttonClick() {
+  document.getElementById("inc").value = --i;
+}
--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -1,10 +1,16 @@
 // Do not change
-document.getElementById('cat').addEventListener('click', () => {
-  alert('meow!');
+document.getElementById("cat").addEventListener("click", () => {
+  alert("meow!");
 });
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
 
+element.addEventListener("click", function () {
+  document.addEventListener("more-info").innerHTML = "Here's some info";
+});
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+element.addEventListener("click", function () {
+  document.addEventListener("bark").innerHTML = "Bow wow!";
+});
