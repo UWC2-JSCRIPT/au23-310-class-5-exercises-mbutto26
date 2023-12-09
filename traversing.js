011e17571cb941f272b92b6488bc74a0ac2a38e8
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2023-12-08 17:03:53 -0800

    Update traversing

--- traversing.js
+++ traversing.js
@@ -1,15 +1,16 @@
 // Given the <body> element as variable body,
 // access the <main> node and log to the console.
-const mainNode = document.body;
+const mainNode = document.querySelector("main");
 console.log(mainNode);
 // Given the <ul> element as variable ul,
 // access <body>  and log to the console.
 
-const childElement = document.getElementsByName("ul");
-const bodyElement = childElement.previousSibling;
+const childElement = document.querySelector("ul");
+const bodyElement = childElement.parentNode.parentNode;
 console.log(bodyElement);
 // Given the <p> element as var p,
 // access the 3rd <li>  and log to the console.
-const siblingElement = document.getElementsByTagName(`li`)[3];
-siblingElement.childNodes;
-console.log(siblingElement);
+const siblingElement = document.querySelector("p");
+const lastEl = siblingElement.previousElementSibling.lastElementChild;
+//const lastEl = siblingElement.childNode;
+console.log(lastEl);
